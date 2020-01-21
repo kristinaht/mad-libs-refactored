9e5029a8b333c9dee7f399f8a9d89f5b98711062
Student <example@example.com>
2020-01-21 15:08:52 -0800

    this is the same mad-lips app ,but with a refactored js code for foreach

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,43 @@
-$(document).ready(function()	{
-	$("#formOne").submit(function()	 {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput = $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val(); 
-   
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+$
 
-    $("#story").show();
+//Below is the app logic using a regular Jquery function 
+
+// (document).ready(function()	{
+// 	$("#formOne").submit(function()	 {
+//     var person1Input = $("input#person1").val();
+//     var person2Input = $("input#person2").val();
+//     var animalInput = $("input#animal").val();
+//     var exclamationInput = $("input#exclamation").val();
+//     var verbInput = $("input#verb").val();
+//     var nounInput = $("input#noun").val(); 
+   
+//     $(".person1").text(person1Input);
+//     $(".person2").text(person2Input);
+//     $(".animal").text(animalInput);
+//     $(".exclamation").text(exclamationInput);
+//     $(".verb").text(verbInput);
+//     $(".noun").text(nounInput);
+
+//     $("#story").show();
+
+//     event.preventDefault();
+// 	});
+// });
+
+
+//Below is the app logic using a loops and arrays
+
+$(document).ready(function() {
+    $("form#blanks").submit(function(event) {
+        var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+
+        blanks.forEach(function(blank) {
+            var userInput = $("input#" + blank).val();
+            $("." + blank).text(userInput);
+        });
+        $("#story").show();
 
     event.preventDefault();
-	});
+    
+    });
 });
